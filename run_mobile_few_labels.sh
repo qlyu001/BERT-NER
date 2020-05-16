4aad204b77c5ecd2d8b69bef23c25eaa16833205
qlyu001 <qlyu001@ucr.edu>
2020-05-15 20:35:46 -0700

    updates

--- run_mobile_few_labels.sh
+++ run_mobile_few_labels.sh
@@ -8,9 +8,9 @@
     --do_eval=True   \
     --do_predict=True \
     --data_dir=few_labels   \
-    --vocab_file=mobilebert_data_origin/vocab.txt  \
-    --bert_config_file=mobilebert_data_origin/bert_config.json \
-    --init_checkpoint=mobilebert_data_origin/mobilebert_variables.ckpt   \
+    --vocab_file=../mobilebert_data_origin/vocab.txt  \
+    --bert_config_file=../mobilebert_data_origin/bert_config.json \
+    --init_checkpoint=../mobilebert_data_origin/mobilebert_variables.ckpt   \
     --max_seq_length=128   \
     --train_batch_size=32   \
     --learning_rate=2e-5   \
