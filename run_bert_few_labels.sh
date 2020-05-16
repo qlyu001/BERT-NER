7a112d2cdcbbc626f132b56b1c476b65b984bbe3
qlyu001 <qlyu001@ucr.edu>
2020-05-15 20:11:30 -0700

    udpates

---
+++ run_bert_few_labels.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+  CUDA_VISIBLE_DEVICES=0 python BERT_NER.py\
+    --task_name="NER"  \
+    --do_lower_case=False \
+    --crf=True \
+    --do_train=True   \
+    --do_eval=false   \
+    --do_predict=True \
+    --data_dir=few_labels   \
+    --vocab_file=cased_L-12_H-768_A-12/vocab.txt  \
+    --bert_config_file=cased_L-12_H-768_A-12/bert_config.json \
+    --init_checkpoint=cased_L-12_H-768_A-12/bert_model.ckpt   \
+    --max_seq_length=128   \
+    --train_batch_size=32   \
+    --learning_rate=2e-5   \
+    --num_train_epochs=4.0   \
+    --output_dir=./output/result_dir
+
+
+perl conlleval.pl -d '\t' < ./output/result_dir/label_test.txt
+
+
---
+++ run_mobile_few_labels.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+  CUDA_VISIBLE_DEVICES=0 python BERT_NER_MOBILE.py\
+    --task_name="NER"  \
+    --do_lower_case=False \
+    --crf=True \
+    --do_train=True   \
+    --do_eval=True   \
+    --do_predict=True \
+    --data_dir=few_labels   \
+    --vocab_file=mobilebert_data_origin/vocab.txt  \
+    --bert_config_file=mobilebert_data_origin/bert_config.json \
+    --init_checkpoint=mobilebert_data_origin/mobilebert_variables.ckpt   \
+    --max_seq_length=128   \
+    --train_batch_size=32   \
+    --learning_rate=2e-5   \
+    --num_train_epochs=4.0   \
+    --output_dir=./output/result
+
+
+perl conlleval.pl -d '\t' < ./output/result/label_test.txt
+
